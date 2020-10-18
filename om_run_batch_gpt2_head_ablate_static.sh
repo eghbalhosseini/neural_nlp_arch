45971197fdb4e1477a93f5c80bb196591a8100ae
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-10-18 09:32:15 -0400

    run spec

--- om_run_batch_gpt2_head_ablate_static.sh
+++ om_run_batch_gpt2_head_ablate_static.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name=gpt_ablate
-#SBATCH --array=0-83
-#SBATCH --time=1-12:00:00
+#SBATCH --array=70-83
+#SBATCH --time=20:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=120G
 #SBATCH --mail-type=ALL
